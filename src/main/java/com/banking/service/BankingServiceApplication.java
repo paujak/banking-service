c6d87a351fb779f835b557fd75edf330c75f393e
Paulius Jakutavičius <paulius.jakutavicius@visma.com>
2026-06-25 23:21:22 +0300

    simplified_infra: more simplifications

--- src/main/java/com/banking/service/BankingServiceApplication.java
+++ src/main/java/com/banking/service/BankingServiceApplication.java
@@ -1,12 +1,10 @@
 package com.banking.service;
 
-import com.banking.service.config.BankingProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
-@EnableConfigurationProperties(BankingProperties.class)
 public class BankingServiceApplication {
 
     public static void main(String[] args) {
